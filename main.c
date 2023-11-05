1e28bd6c925efaf63167d2569b4935ee8865d0ba
kravchuk21 <krauchukvlad@gmail.com>
2023-11-05 22:39:16 +0300

    feat: implement grid with random floats

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define WEIDTH 10
+#define HEIGHT 10
+
+float grid[HEIGHT][WEIDTH] = {0};
+
+float rand_float(void) {
+    return (float)rand()/(float)RAND_MAX;
+}
 
 int main(void) {
-    printf("Hello, SmoothLife\n");
+    srand(time(0));
+
+    for (size_t y = 0; y < HEIGHT; ++y) {
+        for (size_t x = 0; x < HEIGHT; ++x) {
+            grid[y][x] = rand_float();
+        }
+    }
+
+    for (size_t y = 0; y < HEIGHT; ++y) {
+        for (size_t x = 0; x < HEIGHT; ++x) {
+            printf("%f ", grid[y][x]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
